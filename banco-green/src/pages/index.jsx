f3cd1fcb10b9cad29ee6475e6c1ae3adf994f97f
Larissa <102563632+larissabrum@users.noreply.github.com>
2022-05-26 13:29:06 -0700

    Update index.jsx
    Add landing page and cards page.

--- banco-green/src/pages/index.jsx
+++ banco-green/src/pages/index.jsx
@@ -2,7 +2,8 @@ import Onboard from "./Onboard";
 import Home from "./Home";
 import NotFound from "./NotFound";
 import TermsOfUse from "./TermsOfUse";
-import Transfer from "./Transfer";
-import Help from "./Help";
+import Help from "./help";
+import LandingPage from "./green-landing-page"
+import Cards from "./cards"
 
-export { Onboard, Home, NotFound, TermsOfUse, Transfer, Help };
+export { Onboard, Home, NotFound, TermsOfUse, Help, LandingPage, Cards };
