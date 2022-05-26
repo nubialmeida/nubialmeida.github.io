962e695896e9be260c7c0a32ac3bd6ab9abd3279
Larissa <102563632+larissabrum@users.noreply.github.com>
2022-05-26 13:28:03 -0700

    Update index,jsx
    Add cards page and landing page.

--- banco-green/src/routes/index.jsx
+++ banco-green/src/routes/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Onboard, Home, NotFound, TermsOfUse, Transfer, Help } from "../pages";
+import { Onboard, Home, NotFound, TermsOfUse, Transfer, Help, LandingPage, Cards } from "../pages";
 
 export default function Router() {
     return (
@@ -10,7 +10,8 @@ export default function Router() {
             <Route path="/home" element={<Home />} />
             <Route path="/terms-of-use" element={<TermsOfUse />} />
             <Route path="/help" element={<Help />} />
-            <Route path="/transfer" element={<Transfer />} />
+            <Route path="/green-landing-page" element={<LandingPage />} />
+            <Route path="/cards" element={<Cards />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     );
